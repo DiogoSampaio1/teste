5e0bb6ea7ee52ccd9cf6738a1b18fa7c40e56d94
Diogo <sampaio.diogo2007@gmail.com>
2025-04-14 15:01:28 +0100

    commit

--- MAIN/scripts.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    const start = document.getElementsByClassName('start')[0];
-    const content = document.getElementsByClassName('avaliações')[0];
-
-    document.body.addEventListener('click', function() {
-        start.style.display = 'none';  
-        content.style.display = 'block'; 
-    });
-};
