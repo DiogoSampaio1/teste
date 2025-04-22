e902b7996f6eed8f87025ec851107cf699e53718
Diogo <sampaio.diogo2007@gmail.com>
2025-04-22 16:23:38 +0100

    commit

--- db/database.sql
+++ db/database.sql
@@ -13,3 +13,20 @@ CREATE TABLE Rooms (
     room_id AUTO_INCREMENT PRIMARY KEY,
     room_name VARCHAR(15)
 );
+
+INSERT INTO Products (product_id, product_name, product_code, product_class)
+VALUES 
+  (1001, 'Cabos Rede Red', 1032131232132, 'ms'),
+  (1002, 'Cabos Rede Brancos', 12276523546230, 'ms'),
+  (1003, 'Cabos Rede Amarelos', 1005464535340, 'nssss'),
+  (1020, 'Cabos Rede Pretos', 545235634634646, 'nao ha');
+
+INSERT INTO Acess (ist_number)
+VALUES
+  (430453),
+  (430452);
+
+INSERT INTO Rooms (room_id, room_name)
+VALUES
+  (1, 2.24),
+  (2, 0.59);
