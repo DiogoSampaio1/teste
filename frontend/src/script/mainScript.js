8707362dbd77719737351259ac582112a8d117d3
Isaac Lana <147334111+Isaaclana@users.noreply.github.com>
2025-04-23 10:45:34 +0100

    commit

---
+++ frontend/src/script/mainScript.js
@@ -0,0 +1,15 @@
+// Sidebar
+function openSidebar() {
+    let icon = document.getElementById('ham-men');
+    let sidebar = document.getElementById('nav');
+
+    sidebar.classList.toggle('active');
+
+    if (sidebar.classList.contains('active')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-xmark');
+    } else {
+        icon.classList.remove('fa-xmark');
+        icon.classList.add('fa-bars');
+    }
+};
