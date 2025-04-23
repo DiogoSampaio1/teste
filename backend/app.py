114db1cc881529587a8b2ea118a6594c2cd0506f
Diogo <sampaio.diogo2007@gmail.com>
2025-04-23 12:37:55 +0100

    commit changes

--- backend/app.py
+++ backend/app.py
@@ -67,6 +67,105 @@ def delete_products():
         print("Erro ao deletar produto:", e)
         return jsonify({'error': str(e)}), 500
 
+# ===================================== ROOMS ======================================= #
+
+@app.route('/room', methods=['GET'])
+def get_products():
+    query = text("""
+    SELECT 
+        * FROM Rooms
+     """)
+    
+    try:
+        with engine.connect() as con: 
+            result = con.execute(query)
+            products = []
+
+            for row in result:
+                products.append({
+                    'room_id': row[0],
+                    'room_name': row[1],
+                })
+
+        return jsonify(products), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/room', methods=['DELETE'])
+def delete_products():
+    room_name = request.args.get('room_name')
+
+    if not room_name:
+        return jsonify({'message': 'Por favor adicione o Nome da Sala que deseja eliminar'}), 400
+
+    try:
+        with engine.begin() as con:
+
+            query_check = text("SELECT * FROM Products WHERE room_name = :room_name")
+            result = con.execute(query_check, {'room_name': room_name}).fetchone()
+
+            if not result:
+                return jsonify({'message': 'Sala não encontrada'}), 404
+
+            query_delete = text("DELETE FROM Products WHERE room_name = :room_name")
+            con.execute(query_delete, {'room_name': room_name})
+
+        return jsonify({'message': 'Sala eliminada com sucesso!'}), 200
+
+    except Exception as e:
+        print("Erro ao eliminar sala:", e)
+        return jsonify({'error': str(e)}), 500
+
+# ===================================== Users ======================================= #
+
+@app.route('/user', methods=['GET'])
+def get_products():
+    query = text("""
+    SELECT 
+        * FROM Access
+     """)
+    
+    try:
+        with engine.connect() as con: 
+            result = con.execute(query)
+            products = []
+
+            for row in result:
+                products.append({
+                    'ist_number': row[0],
+                })
+
+        return jsonify(products), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/user', methods=['DELETE'])
+def delete_products():
+    ist_number = request.args.get('ist_number')
+
+    if not ist_number:
+        return jsonify({'message': 'Por favor adicione o IST Number que deseja retirar o acesso'}), 400
+
+    try:
+        with engine.begin() as con:
+
+            query_check = text("SELECT * FROM Products WHERE ist_number = :ist_number")
+            result = con.execute(query_check, {'ist_number': ist_number}).fetchone()
+
+            if not result:
+                return jsonify({'message': 'Acesso não encontrado'}), 404
+
+            query_delete = text("DELETE FROM Products WHERE ist_number = :ist_number")
+            con.execute(query_delete, {'ist_number': ist_number})
+
+        return jsonify({'message': 'Acesso retirado com sucesso!'}), 200
+
+    except Exception as e:
+        print("Erro ao retirar acesso:", e)
+        return jsonify({'error': str(e)}), 500
+     
 # ===================================== ROUTE ======================================= #   
     
 @app.route('/')
