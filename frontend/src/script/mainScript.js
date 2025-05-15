a0147a22cb488b884374094c08475c8294c943f0
Diogo <sampaio.diogo2007@gmail.com>
2025-05-15 14:39:15 +0100

    c

--- frontend/src/script/mainScript.js
+++ frontend/src/script/mainScript.js
@@ -25,7 +25,7 @@ function openSidebar() {
     }
 };
 
-const URL = 'scanwithme.netlify.app';
+const URL = 'https://d970-193-136-166-20.ngrok-free.app';
 
 function userPlace() {
   const userDiv = document.getElementById('user-name');
