96106adc1a22e207093725e84ad02e944ac6f946
Diogo <sampaio.diogo2007@gmail.com>
2025-05-15 11:47:53 +0100

    c

--- backend/app.py
+++ backend/app.py
@@ -55,7 +55,6 @@ def generate_random_password(length=28):
 
 #GET PRODUCTS
 @app.route('/products', methods=['GET'])
-@jwt_required()
 @swag_from('../swagger/getProducts.yaml')
 def get_products():
     query = text("""
@@ -87,7 +86,6 @@ def get_products():
     
 #POST PRODUCTS
 @app.route('/products', methods=['POST'])
-@jwt_required()
 @swag_from('../swagger/postProducts.yaml')
 def add_product():
     data = request.json
@@ -134,7 +132,6 @@ def add_product():
     
 #PUT PRODUCTS
 @app.route('/products', methods=['PUT'])
-@jwt_required()
 @swag_from('../swagger/putProducts.yaml')
 def update_products():
     with engine.connect() as con:
@@ -170,7 +167,6 @@ def update_products():
         
 #DELETE PRODUCTS
 @app.route('/products', methods=['DELETE'])
-@jwt_required()
 @swag_from('../swagger/deleteProducts.yaml')
 def delete_products():
     product_code = request.args.get('product_code')
@@ -200,7 +196,6 @@ def delete_products():
 
 #GET ROOMS
 @app.route('/room', methods=['GET'])
-@jwt_required()
 @swag_from('../swagger/getRoom.yaml')
 def get_room():
     query = text("""
@@ -226,7 +221,6 @@ def get_room():
     
 #POST ROOMS
 @app.route('/room', methods=['POST'])
-@jwt_required()
 @swag_from('../swagger/postRoom.yaml')
 def add_room():
     data = request.json
@@ -261,7 +255,6 @@ def add_room():
 
 #PUT ROOMS
 @app.route('/room', methods=['PUT'])
-@jwt_required()
 @swag_from('../swagger/putRoom.yaml')
 def update_room():
     with engine.connect() as con:
@@ -291,7 +284,6 @@ def update_room():
 
 #DELETE ROOMS
 @app.route('/room', methods=['DELETE'])
-@jwt_required()
 @swag_from('../swagger/deleteRoom.yaml')
 def delete_room():
     room_name = request.args.get('room_name')
@@ -321,7 +313,6 @@ def delete_room():
 
 #GET USERS
 @app.route('/user', methods=['GET'])
-@jwt_required()
 @swag_from('../swagger/getUser.yaml')
 def get_users():
     query = text("""
@@ -346,7 +337,6 @@ def get_users():
 
 # POST USERS
 @app.route('/user', methods=['POST'])
-@jwt_required()
 @swag_from('../swagger/postUser.yaml')
 def add_user():
     data = request.json
@@ -396,7 +386,6 @@ def add_user():
     
 #DELETE USERS
 @app.route('/user', methods=['DELETE'])
-@jwt_required()
 @swag_from('../swagger/deleteUser.yaml')
 def delete_users():
     ist_number = request.args.get('ist_number')
