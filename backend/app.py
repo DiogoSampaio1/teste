065d22cde3ff1657d7249f48ee1f0c17234f5b48
Diogo <sampaio.diogo2007@gmail.com>
2025-05-15 14:15:18 +0100

    c

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ from datetime import timedelta
 CONFIG_PATH = ''
 #creating app
 app = Flask(__name__)
-CORS(app, origins=["https://scanwithme.netlify.app"])
+CORS(app, supports_credentials=True, origins="*")
 
 app.config['JWT_SECRET_KEY'] = 'teste'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(seconds=20)
