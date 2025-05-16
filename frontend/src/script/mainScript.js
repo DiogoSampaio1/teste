a14ba34b8fcaf6114bd18ca70fcb7dad7b8ef16f
Diogo <sampaio.diogo2007@gmail.com>
2025-05-16 15:21:33 +0100

    c

--- frontend/src/script/mainScript.js
+++ frontend/src/script/mainScript.js
@@ -25,7 +25,7 @@ function openSidebar() {
     }
 };
 
-const URL = 'https://9319-193-136-166-20.ngrok-free.app';
+const URL = 'https://31a0-193-136-166-20.ngrok-free.app';
 
 function userPlace() {
   const userDiv = document.getElementById('user-name');
